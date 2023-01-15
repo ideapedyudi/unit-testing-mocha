78034f066752603a7d53d107e60617e6c0202ebc
ideapedyudi <ideapedyudi@gmail.com>
2023-01-15 10:15:53 +0700

    add unit test add api

---
+++ test/add.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../index');
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('API endpoint test add', () => {
+    it('should add an item on /add POST', (done) => {
+        const item = {
+            name: 'caca',
+            age: 18
+        }
+        chai.request(app)
+            .post('/add')
+            .send(item)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('age');
+                done();
+            });
+    });
+});
