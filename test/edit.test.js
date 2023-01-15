7d8e9f01e574945e638cc1d79d1a7d6bbdf686c9
ideapedyudi <ideapedyudi@gmail.com>
2023-01-15 10:17:55 +0700

    add unit test edit api

---
+++ test/edit.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../index');
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('API endpoint test add', () => {
+    it('should update an item on /edit/:id PUT', (done) => {
+        const item = {
+            name: 'caca tayo',
+            age: 18
+        }
+        chai.request(app)
+            .put('/edit/63c36a7a2059640c667e4e2e')
+            .send(item)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('name').eql('caca tayo');
+                res.body.should.have.property('age').eql(18);
+                done();
+            });
+    });
+});
