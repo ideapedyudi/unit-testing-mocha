aa0a7adb29db429aa967a46a5f17d71bddb6a0a8
ideapedyudi <ideapedyudi@gmail.com>
2023-01-16 16:18:53 +0700

    update scripts run package json

--- test-endpoint/autentikasi.test.js
+++ test-endpoint/autentikasi.test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const mochawesome = require('mochawesome');
-const marge = require('mochawesome-merge');
+const fs = require('fs')
 
 chai.should();
 chai.use(chaiHttp);
@@ -11,24 +10,11 @@ describe('API Endpoint Test', () => {
         chai.request('https://fr24.edifly-si.com')
             .get('/service/api/v1/route?page=1&perPage=10&search=&search2={}&timestamp=1673846423')
             .set({ srawungtoken: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IiIsInBob25lIjoiIiwicm91dGluZyI6W10sImRlbGV0ZWQiOjAsIl9pZCI6IjYzYTQzYjE2M2Y3NjEyMmIzZDlkMjEzNyIsIm5hbWUiOiJTdXBlciBVc2VyIiwibGV2ZWwiOjEzMTA1NiwidXNlcm5hbWUiOiJhZG1pbiIsImNyZWF0ZWRBdCI6IjIwMjItMTItMjJUMTE6MTA6MTQuNjA1WiIsIl9fdiI6MCwiYmVfdmVyc2lvbiI6IjAuMC4wLjAwMDAiLCJpYXQiOjE2NzM4NDY0MTIsImV4cCI6MTY3Mzg2NDQxMiwiYXVkIjoiSEtOZXQiLCJzdWIiOiJMRENTQWlycG9ydCJ9.HLEVIaFRLZBLLk-moGK-fjXdA6GoIAx-9qdrExT55aEYmTs0D6G0jmn49WExb4El0yuWtsOL_O7I-Nc_-NQNgA' })
-            // .end((err, res) => {
-            //     res.should.have.status(200);
-            //     res.body.should.be.a('object');
-            //     done();
-            // })
-            .then((res) => {
-                const body = res.body;
-                // do something with the body
-                const testResult = {
-                    name: 'API Test',
-                    result: body,
-                };
-                //add the testResult to the mochawesome report
-                mochawesome.addContext(testResult);
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                fs.writeFileSync('result/autentikasi.test.json', JSON.stringify(res.body))
                 done();
-            })
-            .catch((err) => {
-                done(err);
             });
     });
 });
